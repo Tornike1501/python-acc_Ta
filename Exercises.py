b5fae7630f2120df4b06adcbaa4dbaf7796b46fe
Tornike1501 <101277873+Tornike1501@users.noreply.github.com>
2022-07-14 23:49:07 +0400

    Update Exercises.py

--- Exercises.py
+++ Exercises.py
@@ -9,7 +9,7 @@ print(len(x))
 print(*x.values());
 
 #Exercise 2
-def biggerIsGreater(w):
+def big(w):
     w = list(w)
     i = len(w) - 1
     while i > 0 and w[i - 1] >= w[i]:
@@ -24,5 +24,31 @@ def biggerIsGreater(w):
     return ''.join(w);
 
 #Exercise 3
+import math
+import os
+import random
+import re
+import sys
+
+def bomber(n,grid):
+    if n== 1:
+        return grid
+    if n%2 ==0:
+        return ['O'*c for i in range (r)]
+    n//=2
+    for k in range ((n+1)%2 +1):
+        newgrid =[['O']*c for i in range(r)]
+        def set (x,y):
+            if 0<=x<= k and 0<=y<=c:
+                newgrid[x][y]='.'
+        xi=[0,0,0,1,-1]
+        yi=[0,-1,1,0,0]
+    for x in range (r):
+        for y in range (c):
+            if grid[x][y]=='O':
+                for i,j in zip (xi,yi):
+                    set (x+i,y+j)
+    grid = newgrid
+    return ["".join(x) for x in grid];
 
 
